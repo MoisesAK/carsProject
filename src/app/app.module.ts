9c13b25165bff92103f3c86d256e335d8b73deb3
MoisesAK <moises.saifell@gmail.com>
2018-12-27 08:52:18 -0200

    colocando o registro e menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { CarsComponent } from './cars/cars.component';
+import { RegisterComponent } from './cars/register/register.component';
+import { UpdateComponent } from './cars/update/update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    CarsComponent,
+    RegisterComponent,
+    UpdateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
